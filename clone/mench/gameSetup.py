62b61fe08bc8b91e6afaba85912f21dfc0b37504
sina <sinanatanaizkandez@gmail.com>
2025-02-04 20:08:26 +0330

    V 1.0

--- clone/mench/gameSetup.py
+++
@@ -1,8 +0,0 @@
-player_quantity = input("How many player will played (2 - 4) : ")
-color = ['red' , 'green' , "yellow" , "blue"]
-playersColor = []
-
-for num in range(1 , int(player_quantity)+1):
-    colorPicked = input(f"which color is player {num} {color} : ").lower()
-    playersColor.append(colorPicked)
-    color.remove(colorPicked)
